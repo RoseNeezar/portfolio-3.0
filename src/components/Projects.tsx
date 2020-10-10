a92777bb4cb32201eb7333d96549a12514ab5984
Neezar <rforlyffe@gmaicl.com>
2020-10-10 22:50:16 +0800

    add project types

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { FC } from "react"
 import Title from "./Title"
 import Project from "./Project"
 import { Link } from "gatsby"
-const Projects = () => {
-  return <h2>projects list</h2>
+import { RegularStrapiProjectFragment } from "../generated/graphql"
+
+interface IProject {
+  projects?: RegularStrapiProjectFragment[]
+  title?: string
+  showLink?: boolean
+}
+const Projects: FC<IProject> = ({ projects, title, showLink }) => {
+  return (
+    <section className="section projects">
+      <Title title={title} />
+      <div className="section-center projects-center">
+        {projects?.map((project, index) => {
+          return <Project key={project.id} index={index} {...project} />
+        })}
+      </div>
+      {showLink && (
+        <Link to="/project" className="btn center-btn">
+          projects
+        </Link>
+      )}
+    </section>
+  )
 }
 
 export default Projects
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { FC, ReactNode } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
@@ -6,13 +6,48 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+import { AllStrapiProjectsFeatureQuery } from "../generated/graphql"
+
+interface PageData<D> {
+  data: D
+}
+
+const Index: FC<PageData<AllStrapiProjectsFeatureQuery>> = (
+  data: PageData<AllStrapiProjectsFeatureQuery>
+) => {
+  const { allStrapiProjects: projects } = data.data
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects.nodes} title="featured projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+export default Index
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
