c238b233f45e376c617eab06f979c4770b30beb7
Neezar <rforlyffe@gmaicl.com>
2020-12-28 14:08:19 +0800

    update projects

--- src/constants/services.tsx
+++ src/constants/services.tsx
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "Frontend development",
-    text: `I'm using react or vue with typescript. When starting a web based project my go to tools if the website has more static pages like a blog or cms as a backend would be gatsbyjs, if it requires good SEO and multiple static and dynamic pages would be nextjs, else if the website content hides behind a login screen, where most page is dynamic, and only the landing pages is static would be gatsbyjs.`,
+    text: `I'm using Nextjs which is a reactjs framework for my projects. I find the server side rendering and the ability to do dynamic import makes nextjs the best tool for building frontend webapps.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "Backend development",
-    text: `I'm using nodejs with typescript as my main backend framework. Mongodb for the database for all of my apps.`,
+    text: `I'm using nodejs with typescript as my main backend framework. Mongodb or postgressql for the database for all of my apps. Depending on use case, my go to api would be graphql and rest`,
   },
   {
     id: 3,
     icon: <FaApple className="service-icon" />,
     title: "Mobile developemnt",
-    text: `I'm using React native with typescript `,
+    text: `I'm using React native with typescript . Expo managed for less native api reliant apps and expo bare for more all around app functionality`,
   },
 ]
